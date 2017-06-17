995597b82e57e6c6edac28c61b5abfa1030c69e8
AnujaySaraf <AnujaySaraf@users.noreply.github.com>
2017-06-17 02:17:56 +0000

    Done

---
+++ build.py
@@ -0,0 +1,16 @@
+def has_unique_chars(inputString):
+    count = 0
+    if(inputString == None):
+        return False
+    elif(inputString == ""):
+        return True
+    else:
+        for i in inputString:
+            print inputString.count(i)
+            if(inputString.count(i) > 1):
+                return False
+                count += count
+                break
+        if count == 0:
+            return True
+has_unique_chars("foo")
